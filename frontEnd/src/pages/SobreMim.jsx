6de12a138b27cb9aeac09a006a746efb7a345344
Hfaaf <heitorfariaspinheirodoamaral@gmail.com>
2025-05-04 22:22:44 -0300

    AjustesDeResponividade:{Tela:SobreMim}

--- frontEnd/src/pages/SobreMim.jsx
+++ frontEnd/src/pages/SobreMim.jsx
@@ -35,8 +35,8 @@ export function SobreMim() {
                                 Experiência profissional:
                             </span>
                         </h2>
-                        <div className="pt-5 relative overflow-x-auto shadow-md sm:rounded-lg">
-                            <table className="w-full max-w-4xl text-sm text-left text-gray-500 dark:text-gray-400">
+                        <div className="pt-5 relative overflow-x-auto shadow-md sm:rounded-lg w-full">
+                            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
                                         <th scope="col" className="px-6 py-3">
